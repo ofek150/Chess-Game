a8540fbdf7df848538a12ee14fddb1d59c9ba58e
unknown <daniel.mordo06@gmail.com>
2022-12-11 17:04:18 +0200

    Implemented king's canMove

--- Ex7-8Files/King.cpp
+++ Ex7-8Files/King.cpp
@@ -6,5 +6,12 @@ King::~King() {} // DTOR
 
 bool King::canMove(Figure* (board)[BOARD_SIZE][BOARD_SIZE], const Move& move) const  // Returns true if the King can move and false if it can't
 {
+	int srcX = move.getSrc().getX(), srcY = move.getSrc().getY(), destX = move.getDest().getX(), destY = move.getDest().getY();
+    int deltaX = std::abs(srcX - destX);
+    int deltaY = std::abs(srcY - destY);
+    if (deltaX > 1 || deltaY > 1)
+    {
+        return false;
+    }
     return true;
 }
