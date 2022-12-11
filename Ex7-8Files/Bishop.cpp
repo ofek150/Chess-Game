b0143fcbbee60e02ebdbc484a50c29e31f0664ef
ofek150 <ofek080306@gmail.com>
2022-12-11 18:50:56 +0200

    Implemented canMove of Bishop

--- Ex7-8Files/Bishop.cpp
+++ Ex7-8Files/Bishop.cpp
@@ -6,5 +6,27 @@ Bishop::~Bishop() {} // DTOR
 
 bool Bishop::canMove(Figure* (board)[BOARD_SIZE][BOARD_SIZE], const Move& move) const // Returns true if the Bishop can move and false if it can't
 {
+    int srcX = move.getSrc().getX(), srcY = move.getSrc().getY(), destX = move.getDest().getX(), destY = move.getDest().getY();
+    int deltaX = srcX - destX;
+    int deltaY = srcY - destY;
+    int dirX = (deltaX > 0) ? -1 : 1;
+    int dirY = (deltaY > 0) ? -1 : 1;
+
+    if (abs(deltaX) != abs(deltaY))
+    {
+        return false;
+    }
+    int x = srcX + dirX;
+    int y = srcY + dirY;
+    while (x != destX && y != destY)
+    {
+        if (board[y][x]->getType() != "EmptySlot")
+        {
+            return false;
+        }
+        x += dirX;
+        y += dirY;
+    }
+
     return true;
 }
